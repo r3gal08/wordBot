832460378999b4f067283801ebd3ad961b6ebe57
r3gal08 <brettleard808@gmail.com>
2025-02-27 22:34:18 -0400

    - moved ioutil non-deprecated io - Adjusted error handling and messages

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-    "io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
@@ -20,7 +20,7 @@ type WordRequest struct {
 }
 
 type WordResponse struct {
-    Word    string `json:"word"`
+	Word       string `json:"word"`
 	Definition string `json:"definition"`
 }
 
@@ -43,85 +43,87 @@ type DefinitionResponse []struct {
 	} `json:"meanings"`
 }
 
+// This should either return http errors or a queried word info
+// We will want to turn this into an API realistically
 // wordHandler route (writer and a reader)
 func wordHandler(w http.ResponseWriter, r *http.Request) {
 
-    // Reject anything that is not a POST request
-    // Go encourages explict API designs.....
+	// Reject anything that is not a POST request
+	// Go encourages explict API designs.....
 	if r.Method != http.MethodPost {
+		log.Printf("D'oh: StatusMethodNotAllowed")
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-    // decode the JSON request body into req's memory address
+	// SecTODO: Will want to sanitize or validate input here to prevent log injection...
+	// Decode the JSON request body into req's memory address
 	var req WordRequest
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&req); err != nil {
+		log.Printf("D'oh: %v", err)
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
-	defer r.Body.Close()                        // Free resources **after** wordHandler function is executed. Fun fact: Goes garbage collector is concurrent
-  	log.Printf("Received word: %s", req.Word)   // SecTODO: Will want to sanitize or validate input here to prevent log injection...
+	defer r.Body.Close() // Free resources **after** wordHandler function is executed. Fun fact: Goes garbage collector is concurrent
 
-    definition, err := getWordDefinition(req.Word)
-    if err != nil {
-        http.Error(w, "Error getting definition", http.StatusInternalServerError)
-        log.Printf("Error getting definition: %v", err)
-        return
-    }
+	definition, err := getWordDefinition(req.Word)
+	if err != nil {
+		log.Printf("D'oh: %v", err)
+		http.Error(w, "Error getting word definition", http.StatusInternalServerError)
+		return
+	}
 
-    // Craft HTTP response...
-    rsp := WordResponse{
-        Word:       req.Word,
-        Definition: definition,
-    }
+	// Craft HTTP response...
+	rsp := WordResponse{
+		Word:       req.Word,
+		Definition: definition,
+	}
 
-    w.Header().Set("Content-Type", "application/json")  // set writer header content type (in this case json)
-    w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json") // set writer header content type (in this case json)
+	w.WriteHeader(http.StatusOK)
 
-    if err := json.NewEncoder(w).Encode(rsp); err != nil {
-        log.Printf("Error encoding JSON response: %v", err)
-        http.Error(w, "D'oh!", http.StatusInternalServerError)
-        return
-    }
-    log.Printf("Response sent successfully: %v", rsp)
+	if err := json.NewEncoder(w).Encode(rsp); err != nil {
+		log.Printf("D'oh: %v", err)
+		http.Error(w, "Error encoding Dictionary API JSON response", http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Dictionary API Response sent successfully: %v", rsp)
 }
 
 // TODO: We will likely want some kind of "did you mean?" functionality to account for spelling mistakes.....
 // TODO: Improve error handeling and error messages
-func getWordDefinition(word string)(string, error) {
-    url := fmt.Sprintf("https://api.dictionaryapi.dev/api/v2/entries/en/%s", word)
+func getWordDefinition(word string) (string, error) {
+	url := fmt.Sprintf("https://api.dictionaryapi.dev/api/v2/entries/en/%s", word)
 
-    response, err := http.Get(url)
-    if err != nil {
-        return "", err
-    }
-    defer response.Body.Close()
+	response, err := http.Get(url)
+	if err != nil || response.StatusCode != http.StatusOK {
+		if err == nil {
+			err = fmt.Errorf("API returned status code: %d", response.StatusCode)
+		}
+		return "", err
+	}
+	defer response.Body.Close()
 
-    // TODO: Could we not combine this status code with the previous error handling?
-    if response.StatusCode != http.StatusOK {
-        return "", fmt.Errorf("API returned status code: %d", response.StatusCode)
-    }
+	// Dynamically allocate and read data stream. Returns []byte slice and err value
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err // Curious what err actually looks like. Test this by inducing an error sometime
+	}
 
-    // Dynamically allocate and read data stream. Returns []byte slice and err value
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        return "", err  // Curious what err actually looks like. Test this by inducing an error sometime
-    }
+	var definitionResponse DefinitionResponse
+	if err := json.Unmarshal(body, &definitionResponse); err != nil {
+		return "", err
+	}
 
-    var definitionResponse DefinitionResponse
-    if err := json.Unmarshal(body, &definitionResponse); err != nil {
-        return "", err
-    }
+	// TODO: This should be a seperate method likely that returns specific information
+	// Avoid runtime panic by insuring an index-out-of-bound error does not occur and return the first definition
+	if len(definitionResponse) > 0 && len(definitionResponse[0].Meanings) > 0 && len(definitionResponse[0].Meanings[0].Definitions) > 0 {
+		def := definitionResponse[0].Meanings[0].Definitions[0].Definition
+		return def, nil
+	}
 
-    // Avoid runtime panic by insuring an index-out-of-bound error does not occur and return the first definition
-    if len(definitionResponse) > 0 && len(definitionResponse[0].Meanings) > 0 && len(definitionResponse[0].Meanings[0].Definitions) > 0 {
-        def := definitionResponse[0].Meanings[0].Definitions[0].Definition
-        log.Printf("Definition: %s", def)
-        return def, nil
-    }
-
-    return "", fmt.Errorf("definition not found for word: %s", word)
+	return "", fmt.Errorf("Definition not found for word: %s", word)
 }
 
 func main() {
