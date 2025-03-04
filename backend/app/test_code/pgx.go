d9d34351ed331409f6577c3048c40387f068e263
r3gal08 <brettleard808@gmail.com>
2025-03-04 00:12:10 -0400

    - Docker compose file deploys a basic postgres DB - Added pgx example code

---
+++ backend/app/test_code/pgx.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx"
+)
+
+// Test code for connecting to a PostgreSQL database using pgx
+// This code is not used in the application
+func main() {	
+	// Connect to the database
+	// TODO: Un-hardcode the connection string
+	connStr := "postgres://postgres:test@localhost:5432"
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Test the connection
+	var greeting string
+	err = conn.QueryRow(context.Background(), "SELECT 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		log.Fatalf("QueryRow failed: %v\n", err)
+	}
+
+	fmt.Println(greeting)
+}
